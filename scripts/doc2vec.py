fdf1fc8f13665e6b3aebd15ac34381aa4db51861
benedictb <becker.benedictj@gmail.com>
2017-12-14 20:20:36 -0500

    ok model actually works now

--- scripts/doc2vec.py
+++ scripts/doc2vec.py
@@ -1,5 +1,5 @@
 import glob
-import random
+import pickle
 
 from gensim.models import doc2vec
 from collections import namedtuple, Counter
@@ -10,63 +10,62 @@ import tqdm
 
 def make_tokens(file):
     print('Tokenizing...')
-    return [tokenize(f.strip('\n'), lower=True, deacc=True) for f in open(file).readlines()]
-
-    # with open('./dat/all/gen{}.tokenize'.format(i), 'w+') as f:
-    #     for l in lines:
-    #         f.write(l + '\n')
+    hey = [list(tokenize(f.strip('\n'), lower=True, deacc=True)) for f in open(file).readlines()]
+    print('DONE')
+    return hey
 
 
 def build_vocab_er(file_list):
-    c = Counter
-    for file in tqdm.tqdm(file_list, total=len(file_list)):
-        for l in open(file):
-            c.update(l.strip('\n').split(' '))
+    try:
+        with open('vocab.p', 'rb') as handle:
+            c = pickle.load(handle)
+    except OSError:
+        c = Counter()
+        for file in tqdm.tqdm(file_list, total=len(file_list)):
+            for l in open(file):
+                c.update(l.strip('\n').split(' '))
+        with open('vocab.p', 'wb') as handle:
+            pickle.dump(c, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    del c['']
+    return c
 
 
 def make_model(epochs=10):
     model = doc2vec.Doc2Vec(size=100,  # Model initialization
                             window=8,
                             min_count=10,
-                            workers=4,
-                            docvecs_mapfile='./mapfile.map')
+                            workers=4)
+    # docvecs_mapfile='./mapfile.map')
 
     analyzedDocument = namedtuple('AnalyzedDocument', 'words tags')
-
-    alpha_val = 0.025  # Initial learning rate
-    min_alpha_val = 1e-4  # Minimum for linear learning rate decay
-    alpha_delta = (alpha_val - min_alpha_val) / (epochs - 1)
 
     # This is gon' be big
     F = 0
 
     # Deterministic for labels
     file_list = glob.glob('./dat/billion_corpus/all/*')
+    # file_list = ['./test.txt']
     print(file_list)
 
     model.build_vocab_from_freq(build_vocab_er(file_list))
 
-    for e in range(epochs):
+    I = 0
+    for file in file_list:
 
-        model.alpha, model.min_alpha = alpha_val, alpha_val
-        I = 0
-        for file in file_list:
+        docs = []
+        lines = make_tokens(file)
 
-            docs = []
-            lines = make_tokens(file)
+        for l in tqdm.tqdm(lines, total=len(lines)):
+            tags = [I]
+            docs.append(analyzedDocument(l, tags))
+            I += 1
 
-            for l in tqdm.tqdm(lines, total=len(lines)):
-                tags = [I]
-                docs.append(analyzedDocument(l, tags))
-                I += 1
-
-            model.train(docs)
-            print('Files Trained: {}'.format(F))
-            F += 1
-            print('Saving')
-            model.save('model.gensim')
-        alpha_val -= alpha_delta
-        print('Epochs: {}'.format(e))
+        model.train(docs, total_words=10000000, epochs=epochs )
+        print('Files Trained: {}'.format(F))
+        F += 1
+        print('Saving')
+        model.save('model.gensim')
 
 
 if __name__ == '__main__':
